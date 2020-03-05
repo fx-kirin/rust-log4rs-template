8cf4bc3f746c2f6cff0f3d42287f500ce2155c2a
fx_kirin <fx.kirin@gmail.com>
2020-03-05 14:44:12 +0900

    fix log4rs

--- {{cookiecutter.package_name}}/tests/test_lib.rs
+++ {{cookiecutter.package_name}}/tests/test_lib.rs
@@ -1,7 +1,40 @@
 #[cfg(test)]
 mod tests {
+    fn find_log_config_directory<'a>(exe_path: &'a PathBuf) -> &'a Path {
+        let mut exe_path: &std::path::Path = exe_path;
+        loop {
+            exe_path = match exe_path.parent() {
+                Some(exe_path) => exe_path,
+                None => panic!("failed to get parent path"),
+            };
+            if exe_path.join("config").exists() {
+                break;
+            }
+        }
+        exe_path
+    }
+
     #[test]
     fn it_works() {
+        let exe_path = match env::current_exe() {
+            Ok(exe_path) => exe_path,
+            Err(e) => panic!("failed to get current exe path: {}", e),
+        };
+        let exe_path = find_log_config_directory(&exe_path);
+        if cfg!(debug_assertions) {
+            log4rs::init_file(
+                exe_path.join("config/debug/log4rs.yaml"),
+                Default::default(),
+            )
+            .unwrap();
+        } else {
+            log4rs::init_file(
+                exe_path.join("config/release/log4rs.yaml"),
+                Default::default(),
+            )
+            .unwrap();
+        }
+
         assert_eq!(2 + 2, 4);
     }
 }
